cf400decf881c9f5d2b28c514488cc93d3ae6373
Jake Dibbert <jake.dibbert@sofology.co.uk>
2021-02-05 14:30:20 +0000

    Keys added to JS

---
+++ apps.js
@@ -0,0 +1,12 @@
+document.addEventListener("keydown", function (event) {
+    console.log("HELLO");
+    let keyEvent = document.getElementById("keyEvent");
+    keyEvent.textContent = `${event.which}`;
+    let key = document.getElementById("eventKey");
+    key.textContent = `${event.key}`;
+    let code = document.getElementById("eventCode");
+    code.textContent = `${event.code}`;
+    let which = document.getElementById("eventWhich");
+    which.textContent = `${event.which}`;
+    
+})

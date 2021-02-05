481d37990550e20db2396e4c60be8caf1b6dbcd4
Jake Dibbert <jake.dibbert@sofology.co.uk>
2021-02-05 17:14:55 +0000

    #js code added

--- apps.js
+++ apps.js
@@ -1,4 +1,6 @@
 document.addEventListener("keydown", function (event) {
+    document.getElementById("events").style.visibility ="visible";
+    document.getElementById("viewCode").style.visibility ="visible";
     let keyEvent = document.getElementById("keyEvent");
     keyEvent.textContent = `${event.which}`;
     let key = document.getElementById("eventKey");
@@ -7,6 +9,4 @@ document.addEventListener("keydown", function (event) {
     code.textContent = `${event.code}`;
     let which = document.getElementById("eventWhich");
     which.textContent = `${event.which}`;
-    
-    
 })
